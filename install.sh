ccdf08228de6a21243ce165ce956d975ba48deba
raphaelwb <raphaelwb@gmail.com>
2024-01-04 07:23:57 -0300

    install

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl  https://raw.githubusercontent.com/raphaelwb/TED_MCOM/main/requeriments.txt > requeriments.txt
+curl  https://raw.githubusercontent.com/raphaelwb/TED_MCOM/main/requirements.txt > requirements.txt
 curl  https://raw.githubusercontent.com/raphaelwb/TED_MCOM/main/main.py > main.py
 python3 -m venv venv
 source venv/bin/activate
