e713ea8d560178a388ccee0e399341b719c29c4d
raphaelwb <raphaelwb@gmail.com>
2024-01-04 07:23:16 -0300

    install

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+curl  https://raw.githubusercontent.com/raphaelwb/TED_MCOM/main/requeriments.txt > requeriments.txt
+curl  https://raw.githubusercontent.com/raphaelwb/TED_MCOM/main/main.py > main.py
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
