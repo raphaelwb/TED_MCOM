bccfd00860c381c37c35bdae96023ea7828e723f
raphaelwb <raphaelwb@gmail.com>
2024-01-04 17:45:53 -0300

    dialog view, others

--- main.py
+++ main.py
@@ -16,15 +16,15 @@ import json
 import math 
 import sys
 from pathlib import Path
+import logging
 
 class PrepareData:
 
     def __init__(self):
-        self.config_dir = str(Path.home())+"/.mcom"
+        self.config_dir = os.path.join(Path.home(),".mcom")
         if os.path.isdir(self.config_dir) == False:
             os.mkdir(self.config_dir)
-       
-
+           
     def addElement(self,ele,set):
         itens = []
         keys = ele.split(",")
@@ -36,7 +36,7 @@ class PrepareData:
 
     def verifyVersion(self):
 
-        print("Downloading")
+        logging.info("Downloading")
 
         url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTpv6TQ8wL0mbc0bxCBbvY4_rVllkFQoTyfPozXGHDCPpj-w193UES9oBGgjlxRbccb-jKIDAVPDSzF/pub?output=csv'
         
@@ -57,28 +57,36 @@ class PrepareData:
         version = str(self.df.iloc[0]["Versão"])
         version = version.replace(".","_")
 
-        print("Current Version:",version)
+        logging.info(f"Current Version:{version}")
 
         self.versionCSVFile = os.path.join(self.config_dir,version+".csv")
 
         needUpdate = not(os.path.isfile(self.versionCSVFile))
 
-        print("Updating:",needUpdate)
+        logging.info(f"Updating:{needUpdate}",)
 
         return needUpdate
+    
+    def toText(self,languages):
+        print(languages)
+        txt = ""
+        for i in range(len(languages)-1):
+            txt += languages[i] + ","
+        txt += languages[len(languages)-1]
+        return txt
 
     def update(self):
 
         if ( self.verifyVersion() ):
 
-            print("Creating Database")
+            logging.info("Creating Database")
 
             dbFile = os.path.join(self.config_dir,"mcom.db")
 
             try:
                 os.remove(dbFile)
-            except OSError:
-                pass
+            except OSError as e:
+                logging.critical(e, exc_info=True) 
 
             keys = set()
             language = set()
@@ -134,7 +142,7 @@ class PrepareData:
             sql = "CREATE TABLE data_hardware (id INTEGER PRIMARY KEY AUTOINCREMENT, data_id INTEGER, hardware_id INTEGER, FOREIGN KEY(data_id) REFERENCES data(id), FOREIGN KEY(hardware_id) REFERENCES hardware(id))"
             cur.execute(sql)
 
-            sql = "CREATE TABLE data(id INTEGER PRIMARY KEY AUTOINCREMENT, code TEXT, title TEXT, description TEXT, type INTEGER, data TEXT)"
+            sql = "CREATE TABLE data(id INTEGER PRIMARY KEY AUTOINCREMENT, code TEXT, title TEXT, description TEXT, type INTEGER, data TEXT, languages TEXT)"
 
             cur.execute(sql)
 
@@ -143,7 +151,8 @@ class PrepareData:
             hardware = list(hardware)
 
             for o in objects:
-                sql = "INSERT INTO data(code,title,description,type,data) VALUES('%s','%s','%s','%s','%s')" % (o["code"],o["title"],o["description"],o["type"],o["data"])
+                sql = "INSERT INTO data(code,title,description,type,data,languages) VALUES('%s','%s','%s','%s','%s','%s')" % (o["code"],o["title"],o["description"],o["type"],o["data"],self.toText(o["language"]))
+                logging.info(sql)
                 cur.execute(sql)
                 data_id = cur.lastrowid
                 
@@ -165,7 +174,7 @@ class PrepareData:
             con.commit()
             con.close()
 
-            print("Finished")
+            logging.info("Finished")
 
             os.rename(self.dataTempCVSFile, self.versionCSVFile)
 
@@ -186,6 +195,7 @@ import os
 import sqlite3
 from pathlib import Path
 from jproperties import Properties
+from ttkbootstrap.dialogs import Messagebox
 
 class MCOMSearch(ttk.Frame):
 
@@ -195,7 +205,7 @@ class MCOMSearch(ttk.Frame):
     def __init__(self, master):
         super().__init__(master, padding=15)
         self.pack(fill=BOTH, expand=YES)
-
+       
         _path = pathlib.Path().absolute().as_posix()
         self.path_var = ttk.StringVar(value=_path)
         #self.term_var = ttk.StringVar(value='Digite o termo da busca')
@@ -279,16 +289,33 @@ class MCOMSearch(ttk.Frame):
         endswith_opt.pack(side=LEFT)
         endswith_opt.invoke()
 
-    def selectItem(self,a):
+    def showItem(self,a):   
         curItem = self.resultview.focus()
         item = self.resultview.item(curItem)
-        cmd = self.data[item['values'][0]]["data"]
-        if ( item['values'][2] == "Link"):
-            print("Browser:",cmd)
-            webbrowser.open(cmd)
-        else:
-            print("System:",cmd)
-            os.system(cmd)
+        code = item['values'][0]
+        data= MCOMSearch.data[code]
+        logging.info(data)
+        title = data["title"]
+        description = data["description"]
+        languages = data["languages"]
+        
+
+        Text = f"Código: {code}\n"\
+            f"Título: {title}\n"\
+            f"Descrição: {description}\n"\
+            f"Idiomas: {languages}\n"\
+            "\n"\
+            f"Clique Sim para acessar o conteúdo ..."\
+    
+        mb = Messagebox.show_question(Text, title=code)
+        if ( mb != "No" ):
+            cmd = self.data[code]["data"]
+            if ( item['values'][2] == "Link"):
+                logging.info(f"Browser:{cmd}")
+                webbrowser.open(cmd)
+            else:
+                logging.info(f"System: {cmd}")
+                os.system(cmd)
 
     def create_results_view(self):
         self.resultview = ttk.Treeview(
@@ -302,7 +329,7 @@ class MCOMSearch(ttk.Frame):
         self.resultview.heading(0, text='Código', anchor=W)
         self.resultview.heading(1, text='Título', anchor=W)
         self.resultview.heading(2, text='Tipo', anchor=W)
-        #TODO: Colocar uma bandeirinha com os idiomas
+
         self.resultview.column(
             column=0, 
             anchor=W, 
@@ -322,19 +349,20 @@ class MCOMSearch(ttk.Frame):
             stretch=False
         )
 
-        self.resultview.bind('<Double-1>', self.selectItem)
+        self.resultview.bind('<Double-1>', self.showItem)
 
     def on_search(self):
-        search_term = self.term_var.get()
-        print(search_term)
-      
-        if search_term == '':
+        term = self.term_var.get()
+        
+        if term == '':
             return
+        
+        logging.info(f"Search:{term}")
 
         self.search_btn.state(["disabled"]) 
         Thread(
             target=MCOMSearch.search, 
-            args=(search_term,), 
+            args=(term,), 
             daemon=True
         ).start()
         self.progressbar.start(10)
@@ -370,42 +398,50 @@ class MCOMSearch(ttk.Frame):
     def search(term):
         MCOMSearch.set_searching(True)
         MCOMSearch.data = {}
-        print("Opening Database")
+        logging.info("Opening Database")
         config_dir = str(Path.home())+"/.mcom"
         dbFile = os.path.join(config_dir,"mcom.db")
         propFile = os.path.join(config_dir,"conf.properties")
         props = Properties()
-        try:
-            with open(propFile, 'rb') as config_file:
-                props.load(config_file)
-            hardware = props["HARDWARE"].data
-        except:
-            hardware = "ALL"
+
+        check_file = os.path.isfile(propFile)
+
+        if ( check_file == False ):
+             with open(propFile, 'w') as f:
+                f.write('HARDWARE=LINK')
         
-        print(hardware)
+        with open(propFile, 'rb') as config_file:
+            props.load(config_file)
+        hardware = props["HARDWARE"].data
+        
+        logging.info(hardware)
         #TODO resolver o problema de usar multiplas Threads para abrir o DB apenas uma vez
         conn = sqlite3.connect(dbFile)        
         cur = conn.cursor()
 
-        if ( hardware != "ALL"):
-            sql = "Select * from data,data_keys,keys,data_hardware,hardware where data_keys.data_id = data.id and data_keys.key_id = keys.id and "\
-            "data_hardware.hardware_id = hardware.id and data_hardware.data_id = data.id and hardware.text = '"+hardware+"' AND ("\
-            "(title like '%"+term+"%' or description like '%"+term+"%') OR "\
+        if ( hardware == "LINK"):
+            sql = "Select * from data,data_keys,keys where data.type = 0 and data_keys.data_id = data.id and data_keys.key_id = keys.id and "\
+            "((title like '%"+term+"%' or description like '%"+term+"%') OR "\
+            "(data_keys.data_id = data.id and data_keys.key_id = keys.id and keys.text like '%"+term+"%'))"
+        elif ( hardware == "ALL"):
+            sql = "Select * from data,data_keys,keys where data_keys.data_id = data.id and data_keys.key_id = keys.id and "\
+            "((title like '%"+term+"%' or description like '%"+term+"%') OR "\
             "(data_keys.data_id = data.id and data_keys.key_id = keys.id and keys.text like '%"+term+"%'))"
         else:
-            sql = "Select * from data,data_keys,keys where data_keys.data_id = data.id and data_keys.key_id = keys.id and "\
-            "(title like '%"+term+"%' or description like '%"+term+"%') OR "\
-            "(data_keys.data_id = data.id and data_keys.key_id = keys.id and keys.text like '%"+term+"%')"
-
-        print(sql)
+            sql = "Select * from data,data_keys,keys,data_hardware,hardware where data_keys.data_id = data.id and data_keys.key_id = keys.id and "\
+            "data_hardware.hardware_id = hardware.id and data_hardware.data_id = data.id and hardware.text = '"+hardware+"' AND ("\
+            "((title like '%"+term+"%' or description like '%"+term+"%') OR "\
+            "(data_keys.data_id = data.id and data_keys.key_id = keys.id and keys.text like '%"+term+"%')))"
+            
+        logging.info(sql)
         cur.execute(sql)
         rows = cur.fetchall()
         for row in rows:
-            print(row)
+            logging.info(row)
             type = "Link"
             if ( row[4] == 1 ):
                 type = "App"
-            record = {"title":row[2],"type":type,"data":row[5]}
+            record = {"title":row[2],"type":type,"data":row[5],"description":row[3],"languages":row[6]}
             MCOMSearch.data[row[1]] = record
         cur.close()
         conn.close()
@@ -417,19 +453,21 @@ class MCOMSearch(ttk.Frame):
 
 if __name__ == '__main__':
 
+    logging.basicConfig(level=logging.INFO, filename=os.path.join(Path.home(),".mcom","log.txt"), format="%(asctime)s - %(levelname)s - %(message)s")
+
     try:
         data = PrepareData()
         data.update()
-    except:
-        print("Error Updating - Verify Internet")
+    except Exception as e:
+        logging.critical(e, exc_info=True) 
     
     app = ttk.Window("TVBox - TED MCOM", "journal")
     app.resizable(False,False)
     app.geometry("600x400")
     MCOMSearch(app)
-    print("Opening Database")
+    logging.info("Opening Database")
     config_dir = str(Path.home())+"/.mcom"
     dbFile = os.path.join(config_dir,"mcom.db")
     MCOMSearch.conn = sqlite3.connect(dbFile)
-    print("Ready")
+    logging.info("Ready")
     app.mainloop()
