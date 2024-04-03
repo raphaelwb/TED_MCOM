6fbb2e34716630d86cc3ad423a779f05214db10f
Luiz Filipe Bartelega Penha <luizfpenha16@gmail.com>
2024-04-03 16:09:16 -0300

    json adicionado no script

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 curl  https://raw.githubusercontent.com/raphaelwb/TED_MCOM/main/requirements.txt > requirements.txt
 curl  https://raw.githubusercontent.com/raphaelwb/TED_MCOM/main/main.py > main.py
+curl  https://github.com/raphaelwb/TED_MCOM/blob/main/fenixbook_themes.json > fenixbook_themes.json
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
