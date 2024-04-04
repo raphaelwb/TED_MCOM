fc29208b58228f8455dfd55cda526038a7112c8a
Luiz Filipe Bartelega Penha <luizfpenha16@gmail.com>
2024-04-04 13:55:11 -0300

    curl logo-horizontal

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 curl  https://raw.githubusercontent.com/raphaelwb/TED_MCOM/main/requirements.txt > requirements.txt
 curl  https://raw.githubusercontent.com/raphaelwb/TED_MCOM/main/main.py > main.py
 curl  https://raw.githubusercontent.com/raphaelwb/TED_MCOM/main/fenixbook_themes.json > fenixbook_themes.json
-curl https://raw.githubusercontent.com/raphaelwb/TED_MCOM/main/Logo-FenixBook-elemento.png > Logo-FenixBook-elemento.png
+mkdir img
+curl https://raw.githubusercontent.com/raphaelwb/TED_MCOM/main/Logo-FenixBook-elemento.png > ./img/Logo-FenixBook-elemento.png
+curl https://raw.githubusercontent.com/raphaelwb/TED_MCOM/main/Logo-FenixBook-elemento.png > ./img/Logo-FenixBook-horizontal.png
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
