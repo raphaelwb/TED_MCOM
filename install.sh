062573bef091644255c94f9733dd73ef3afe5deb
Luiz Filipe Bartelega Penha <luizfpenha16@gmail.com>
2024-04-04 14:05:55 -0300

    correcao curl imagens

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ curl  https://raw.githubusercontent.com/raphaelwb/TED_MCOM/main/requirements.txt
 curl  https://raw.githubusercontent.com/raphaelwb/TED_MCOM/main/main.py > main.py
 curl  https://raw.githubusercontent.com/raphaelwb/TED_MCOM/main/fenixbook_themes.json > fenixbook_themes.json
 mkdir img
-curl https://raw.githubusercontent.com/raphaelwb/TED_MCOM/main/Logo-FenixBook-elemento.png > ./img/Logo-FenixBook-elemento.png
-curl https://raw.githubusercontent.com/raphaelwb/TED_MCOM/main/Logo-FenixBook-elemento.png > ./img/Logo-FenixBook-horizontal.png
+curl https://raw.githubusercontent.com/raphaelwb/TED_MCOM/main/img/Logo-FenixBook-elemento.png > ./img/Logo-FenixBook-elemento.png
+curl https://raw.githubusercontent.com/raphaelwb/TED_MCOM/main/img/Logo-FenixBook-elemento.png > ./img/Logo-FenixBook-horizontal.png
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
