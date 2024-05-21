0a5627b844410cc23815dbfc08895e8b7a302cd4
Luiz Filipe Bartelega Penha <luizfpenha16@gmail.com>
2024-05-21 13:29:34 -0300

    add desktop entry

---
+++ fenixAppExec.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd fenixApp
+source venv/bin/activate
+python3 main.py
--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 curl  https://raw.githubusercontent.com/raphaelwb/TED_MCOM/main/requirements.txt > requirements.txt
 curl  https://raw.githubusercontent.com/raphaelwb/TED_MCOM/main/main.py > main.py
 curl  https://raw.githubusercontent.com/raphaelwb/TED_MCOM/main/fenixbook_themes.json > fenixbook_themes.json
+curl  https://raw.githubusercontent.com/raphaelwb/TED_MCOM/main/fenixApp.desktop > fenixApp.desktop
+curl  https://raw.githubusercontent.com/raphaelwb/TED_MCOM/main/fenixAppExec.sh > fenixAppExec.sh
 mkdir img
 curl https://raw.githubusercontent.com/raphaelwb/TED_MCOM/main/img/Logo-FenixBook-elemento.png > ./img/Logo-FenixBook-elemento.png
 curl https://raw.githubusercontent.com/raphaelwb/TED_MCOM/main/img/Logo-FenixBook-horizontal.png > ./img/Logo-FenixBook-horizontal.png
